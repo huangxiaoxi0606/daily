9e002d9ca3d40c2aeb13d49c293c8a0bf5fa3a30
hhx03 <1007502956@qq.com>
2019-07-26 17:35:59 +0800

    网易云

---
+++ wxpy.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2019/7/25 14:18
+# @Author : hhx06
+# @Site : 
+# @File : wxpy.py
+# @Software: PyCharm
+
+# 导入模块
+from wxpy import *
+# 初始化机器人，扫码登陆
+def main():
+
+    bot = Bot()
+
+    print('2')
+
+main()
