9642dc731a0bf17af4e49faf42856fedf94b8eb5
hhx03 <1007502956@qq.com>
2019-10-14 09:07:49 +0800

    携程拉萨

--- Lasa.py
+++ Lasa.py
@@ -11,6 +11,19 @@ from bs4 import BeautifulSoup
 
 def main():
     url = 'https://you.ctrip.com/travels/lhasa36/t3-p2.html'
-    response = requests.get(url)
+    headers = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-Language': 'zh-CN,zh;q=0.9',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'}
+
+    response = requests.get(url, headers=headers)
     html_doc = response.text
     soup = BeautifulSoup(html_doc, 'lxml')
+    list = soup.find_all("a", attrs={"class": "journal-item"})
+    # 遍历存储即可
+    for li in list:
+        print(li)
+        exit(0)
+        
+main()
